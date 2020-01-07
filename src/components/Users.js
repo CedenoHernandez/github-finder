9960d3229952683b9a5817912a6d72c01dec541c
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2020-01-07 15:29:54 -0800

    Add search user

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from '../components/UserItem';
 import Spinner from '../components/Spinner';
 import PropTypes from 'prop-types';
