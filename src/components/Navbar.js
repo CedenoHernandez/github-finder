57ee4c6b9578bb77a2563eb87a49b2f2cbb74fe2
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2020-01-07 11:41:23 -0800

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          <i className="fab fa-github"></i> Navbar
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Navbar
