9de49fbf2fe3fa373e54f0f8610c173d0cdd44b5
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2020-01-07 16:53:45 -0800

    Add clear feature to search

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,8 +6,10 @@ export class Search extends Component {
     text: ''
   };
 
-static propTypes = {
+  static propTypes = {
     searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
   };
 
   onChange = e => this.setState({ text: e.target.value });
@@ -19,12 +21,20 @@ static propTypes = {
   };
 
   render() {
+    const { showClear, clearUsers } = this.props;
+
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
           <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange}/>
           <input type="submit" value="Search" className="btn btn-dark btn-block" />
         </form>
+        {showClear && (
+          <button 
+            className="btn btn-light btn-block"
+            onClick={clearUsers}
+          >Clear
+        </button>)}
       </div>
     )
   }
