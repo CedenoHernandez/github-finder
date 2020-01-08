d8f201135a2cdfb5c48b40befd4fb3845889e683
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2020-01-08 10:13:32 -0800

    Add alert state to search

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Users from './components/Users';
 import Search from './components/Search';
+import Alert from './components/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   //Search Github users
@@ -27,6 +29,12 @@ class App extends Component {
   //Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  //Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  }
+
   render() {
     const { users, loading } = this.state;
 
@@ -34,10 +42,13 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search 
             searchUsers={this.searchUsers} 
             clearUsers={this.clearUsers} 
-            showClear={users.length > 0 ? true : false} />
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+            />
           <Users loading={loading} users={users} />
         </div>
       </div>
