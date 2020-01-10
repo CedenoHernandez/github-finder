83515ceee98be00548c90116efc6e78624378560
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2020-01-09 17:23:31 -0800

    Add NotFound page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import User from './components/User';
 import Alert from './components/Alert';
 import Home from './components/Home';
 import About from './components/About';
+import NotFound from './components/NotFound';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -22,6 +23,7 @@ const App = () => {
                   <Route exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
                   <Route exact path='/user/:login' component={User} />
+                  <Route component={NotFound} />
                 </Switch>
               </div>
             </div>
